d65192ca6e2f3bc76b48d97fb4314e6560c13e0e
LucaGGG <gass@byron.ch>
2023-11-14 11:20:45 +0100

    Security und Datenbank ausgebaut

--- src/main/java/ch/hargrave/richard/personmanagement/controller/CityController.java
+++ src/main/java/ch/hargrave/richard/personmanagement/controller/CityController.java
@@ -2,10 +2,7 @@ package ch.hargrave.richard.personmanagement.controller;
 
 import ch.hargrave.richard.personmanagement.base.MessageResponse;
 import ch.hargrave.richard.personmanagement.model.City;
-import ch.hargrave.richard.personmanagement.security.Roles;
 import ch.hargrave.richard.personmanagement.service.CityService;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +13,6 @@ import javax.management.relation.Role;
 import java.util.List;
 
 @RestController
-@SecurityRequirement(name = "bearerAuth")
 @Validated
 public class CityController {
 
@@ -27,35 +23,30 @@ public class CityController {
     }
 
     @GetMapping("api/city")
-    @RolesAllowed(Roles.Viewer)
     public ResponseEntity<List<City>> getAll() {
         List<City> result = cityService.getCities();
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("api/city/{id}")
-    @RolesAllowed(Roles.Viewer)
     public ResponseEntity<City> getOne(@PathVariable Long id) {
         City city = cityService.getCity(id);
         return new ResponseEntity<>(city, HttpStatus.OK);
     }
 
     @PostMapping("api/city")
-    @RolesAllowed(Roles.Admin)
     public ResponseEntity<City> newCity(@Valid @RequestBody City city) {
         City savedCity = cityService.addCity(city);
         return  new ResponseEntity<>(savedCity, HttpStatus.OK);
     }
 
     @PutMapping("api/city/{id}")
-    @RolesAllowed(Roles.Admin)
     public ResponseEntity<City> putCity(@Valid @RequestBody City city, @PathVariable Long id) {
         City savedCity = cityService.updateCity(city, id);
         return new ResponseEntity<>(savedCity, HttpStatus.OK);
     }
 
     @DeleteMapping("api/city/{id}")
-    @RolesAllowed(Roles.Admin)
     public ResponseEntity<MessageResponse> deleteCity(@PathVariable Long id) {
         try {
             return ResponseEntity.ok(cityService.terminateCity(id));
--- src/main/java/ch/hargrave/richard/personmanagement/controller/PersonalInfoController.java
+++ src/main/java/ch/hargrave/richard/personmanagement/controller/PersonalInfoController.java
@@ -2,10 +2,7 @@ package ch.hargrave.richard.personmanagement.controller;
 
 import ch.hargrave.richard.personmanagement.base.MessageResponse;
 import ch.hargrave.richard.personmanagement.model.PersonalInfo;
-import ch.hargrave.richard.personmanagement.security.Roles;
 import ch.hargrave.richard.personmanagement.service.PersonalInfoService;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@SecurityRequirement(name = "bearerAuth")
 @Validated
 public class PersonalInfoController {
 
@@ -26,35 +22,30 @@ public class PersonalInfoController {
     }
 
     @GetMapping("api/personalInfo")
-    @RolesAllowed(Roles.Viewer)
     public ResponseEntity<List<PersonalInfo>> getAll() {
         List<PersonalInfo> result = personalInfoService.getPersonalInfo();
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("api/personalInfo/{id}")
-    @RolesAllowed(Roles.Viewer)
     public ResponseEntity<PersonalInfo> getOne(@PathVariable Long id) {
         PersonalInfo personalInfo = personalInfoService.getPerson(id);
         return new ResponseEntity<>(personalInfo, HttpStatus.OK);
     }
 
     @PostMapping("api/personalInfo")
-    @RolesAllowed(Roles.User)
     public ResponseEntity<PersonalInfo> newPerson(@Valid @RequestBody PersonalInfo personalInfo) {
         PersonalInfo savedPerson = personalInfoService.addPerson(personalInfo);
         return  new ResponseEntity<>(savedPerson, HttpStatus.OK);
     }
 
     @PutMapping("api/personalInfo/{id}")
-    @RolesAllowed(Roles.User)
     public ResponseEntity<PersonalInfo> putPerson(@Valid @RequestBody PersonalInfo personalInfo, @PathVariable Long id) {
         PersonalInfo savedPerson = personalInfoService.updatePerson(personalInfo, id);
         return new ResponseEntity<>(savedPerson, HttpStatus.OK);
     }
 
     @DeleteMapping("api/personalInfo/{id}")
-    @RolesAllowed(Roles.User)
     public ResponseEntity<MessageResponse> deletePerson(@PathVariable Long id) {
         try {
             return ResponseEntity.ok(personalInfoService.terminatePerson(id));
